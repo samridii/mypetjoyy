04b718015c80fa1ce153e6adc7134032fa6357d8
samridii <samridiic@gmail.com>
2025-12-05 12:48:26 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/mypetjoyy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mypetjoyy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mypetjoyy/app.dart';
+
+void main() {
+  runApp(const App());
+}
