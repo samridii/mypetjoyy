a1d9cd014baa5285d6852f4528806fc232251987
samridii <samridiic@gmail.com>
2025-12-15 10:13:06 +0545

    Added Simple HomePage

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -1,60 +1,105 @@
 import 'package:flutter/material.dart';
 
-class HomePageScreen extends StatelessWidget {
-  const HomePageScreen({super.key});
+void main() {
+  runApp(const HomepageScreen());
+}
+
+class HomepageScreen extends StatelessWidget {
+  const HomepageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PawPerfect',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'SF Pro Display',
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  final screens = const [
+    Center(child: Text('Screen 1')),
+    Center(child: Text('Screen 2')),
+    Center(child: Text('Screen 3')),
+    Center(child: Text('Screen 4')),
+  ];
+
+  final icons = [Icons.home, Icons.shopping_cart, Icons.pets, Icons.calculate];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFDFCFB),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: 140,
-              height: 140,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 179, 229, 232),
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 15,
-                    offset: Offset(0, 6),
-                  ),
-                ],
-              ),
-              alignment: Alignment.center,
-              child: const Text(
-                "I AM HOMEPAGE",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 30),
-
-            const Icon(Icons.favorite, size: 70, color: Color(0xFFE57373)),
-
-            const SizedBox(height: 20),
-
-            const Text(
-              "Welcome to the Home!",
-              style: TextStyle(
-                fontFamily: "Fredoka",
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF4A4A4A),
-              ),
-            ),
-          ],
+      body: screens[selectedIndex],
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
+        decoration: _navDecoration(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(
+            icons.length,
+            (index) => _navItem(icons[index], index),
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _navItem(IconData icon, int index) {
+    final isSelected = selectedIndex == index;
+
+    return GestureDetector(
+      onTap: () => setState(() => selectedIndex = index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.blue : const Color(0xFFF3F4F6),
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.4),
+                    blurRadius: 12,
+                    offset: const Offset(0, 4),
+                  ),
+                ]
+              : [],
+        ),
+        child: Icon(
+          icon,
+          size: 26,
+          color: isSelected ? Colors.white : const Color(0xFF6B7280),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _navDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(30),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          blurRadius: 30,
+          offset: const Offset(0, 10),
+        ),
+      ],
     );
   }
 }
