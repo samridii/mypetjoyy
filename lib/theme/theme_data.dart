9e9c7cd561f66583ffaefba4c808b4265c858f7b
samridii <samridiic@gmail.com>
2025-12-19 17:52:30 +0545

    Added Themes

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    scaffoldBackgroundColor: const Color(0xFFE6F0FF),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 226, 237, 253),
     fontFamily: 'Fredoka',
 
     textTheme: const TextTheme(
       headlineLarge: TextStyle(
-        fontSize: 26,
+        fontSize: 27,
         fontWeight: FontWeight.bold,
         color: Color(0xFF1F2937),
       ),
       headlineMedium: TextStyle(
-        fontSize: 20,
+        fontSize: 21,
         fontWeight: FontWeight.w600,
         color: Color(0xFF1F2937),
       ),
-      titleMedium: TextStyle(fontSize: 14, color: Color(0xFF6B7280)),
-      bodyMedium: TextStyle(fontSize: 14, color: Color(0xFF374151)),
+      titleMedium: TextStyle(fontSize: 15, color: Color(0xFF6B7280)),
+      bodyMedium: TextStyle(fontSize: 15, color: Color(0xFF374151)),
     ),
   );
 }
