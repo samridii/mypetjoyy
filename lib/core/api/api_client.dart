7ea32bee0720e6955fd2c5ff4eb8f37764532bec
samridii <samridiic@gmail.com>
2026-01-17 21:34:30 +0545

    Added api client

---
+++ lib/core/api/api_client.dart
@@ -0,0 +1,97 @@
+// lib/core/network/api_client.dart
+import 'package:dio/dio.dart';
+import 'package:dio_smart_retry/dio_smart_retry.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mypetjoyy/core/api/api_endpoints.dart';
+import 'package:mypetjoyy/core/services/storage/secure_storage_service.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+final apiClientProvider = Provider<ApiClient>((ref) => ApiClient(ref));
+
+class ApiClient {
+  late final Dio _dio;
+  final Ref ref;
+
+  ApiClient(this.ref) {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        connectTimeout: ApiEndpoints.connectTimeout,
+        receiveTimeout: ApiEndpoints.receiveTimeout,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      ),
+    );
+
+    _dio.interceptors.add(_AuthInterceptor(ref));
+
+    _dio.interceptors.add(
+      RetryInterceptor(
+        dio: _dio,
+        retries: 3,
+        retryDelays: const [
+          Duration(seconds: 1),
+          Duration(seconds: 2),
+          Duration(seconds: 3),
+        ],
+        retryEvaluator: (error, attempt) =>
+            error.type == DioExceptionType.connectionTimeout ||
+            error.type == DioExceptionType.sendTimeout ||
+            error.type == DioExceptionType.receiveTimeout ||
+            error.type == DioExceptionType.connectionError,
+      ),
+    );
+
+    if (kDebugMode) {
+      _dio.interceptors.add(
+        PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+          responseHeader: false,
+          error: true,
+          compact: true,
+        ),
+      );
+    }
+  }
+
+  Dio get dio => _dio;
+}
+
+class _AuthInterceptor extends Interceptor {
+  final Ref ref;
+
+  _AuthInterceptor(this.ref);
+
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    // Skip auth header for login & register
+    final isAuthEndpoint =
+        options.path.contains('/auth/login') ||
+        options.path.contains('/auth/register');
+
+    if (!isAuthEndpoint) {
+      final token = await ref.read(secureStorageProvider).getToken();
+      if (token != null) {
+        options.headers['Authorization'] = 'Bearer $token';
+      }
+    }
+    handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    if (err.response?.statusCode == 401) {
+      ref.read(secureStorageProvider).deleteToken();
+      // Optional: trigger global logout event / navigation later
+    }
+    handler.next(err);
+  }
+}
