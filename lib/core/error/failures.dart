3f1c7428a4bbc7566799b8ee1521a35cd6283b0e
samridii <samridiic@gmail.com>
2026-01-18 13:02:33 +0545

    Improve failure and error handling

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -2,25 +2,25 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   final String message;
+
   const Failure(this.message);
 
   @override
-  List<Object?> get props => [message];
+  List<Object> get props => [message];
 }
 
-// Local Database Failure
+class ServerFailure extends Failure {
+  const ServerFailure({required String message}) : super(message);
+}
+
+class AuthFailure extends Failure {
+  const AuthFailure({required String message}) : super(message);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure({required String message}) : super(message);
+}
+
 class LocalDatabaseFailure extends Failure {
-  const LocalDatabaseFailure({
-    String message = 'Local database operation failed',
-  }) : super(message);
-}
-
-// API Failure with status code
-class ApiFailure extends Failure {
-  final int? statusCode;
-
-  const ApiFailure({required String message, this.statusCode}) : super(message);
-
-  @override
-  List<Object?> get props => [message, statusCode];
+  const LocalDatabaseFailure({required String message}) : super(message);
 }
